1307a36755a89f017576c46647ffa3884f6c9aab
Harry Holmes <harryholmesse@gmail.com>
2025-09-22 11:53:07 +0200

    Added session deletion functionality using local state

--- GymSlay/src/layouts/MainLayout.jsx
+++ GymSlay/src/layouts/MainLayout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Header } from '../components/header/Header'
+import SessionsPage from '../pages/sessions/SessionsPage'
 
 export const MainLayout = () => {
   return (
-    <Header />
+    <div>
+      <Header />
+      <SessionsPage />
+    </div>
   )
 }
---
+++ GymSlay/src/pages/sessions/SessionsPage.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './SessionsPage.css';
+
+const SessionsPage = () => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [sessionId, setSessionId] = useState(null)
+  const [sessions, setSessions] = useState([
+    { id: 1, title: "Morning Cardio" },
+    { id: 2, title: "Strength Training" }
+  ])
+
+  const getSessionInfo = () => {
+    return sessions.find(session => session.id === sessionId);
+  }
+
+  const openDeleteModal = (id) => {
+    console.log(`Open delete modal for session with id: ${id}`);
+    setSessionId(id);
+    setShowDeleteModal(true);
+  }
+
+  const closeDeleteModal = () => {
+    console.log(`Close delete modal`);
+    setShowDeleteModal(false);
+  }
+
+  const handleDelete = (id) => {
+    console.log(`Deleted session with id: ${id}`);
+    setSessions(sessions.filter(session => session.id !== id));
+    closeDeleteModal();
+  }
+
+  return (
+    <div className="sessions-page">
+      
+      <div className="card-container">
+        {sessions.map(session => (
+          <div key={session.id} className="card">
+            <h3 className="title">{session.title}</h3>
+            <button className='delete-btn' onClick={() => openDeleteModal(session.id)}>X</button>
+          </div>
+        ))}
+      </div>
+    
+      {showDeleteModal && (
+        <div id="delete-modal">
+          <div className='delete-modal-content'>
+            <h2>{getSessionInfo()?.title}</h2>
+            <p>Are you sure you want to delete this session?</p>
+            <div className='delete-modal-actions'>
+              <button className='delete-confirm' onClick={() => {handleDelete(sessionId);}}>Yes</button>
+              <button className='delete-cancel' onClick={closeDeleteModal}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
+
+    </div>
+
+  );
+};
+
+export default SessionsPage;
