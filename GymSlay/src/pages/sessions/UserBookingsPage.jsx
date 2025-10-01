095d59cba1b8ce91555a034d8af887391f9e5aad
Harry Holmes <harryholmesse@gmail.com>
2025-10-01 11:06:39 +0200

    added basic functionality if there is or is not any sessions booked

--- GymSlay/src/pages/sessions/UserBookingsPage.jsx
+++ GymSlay/src/pages/sessions/UserBookingsPage.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './UserBookingsPage.css'
 
 export const UserBookingsPage = () => {
+  // const [bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState([
+    { id: 1, title: "Morning Cardio", date: "2025-10-02", time: "07:00 - 08:00" },
+    { id: 2, title: "Strength Training", date: "2025-10-03", time: "09:00 - 10:00" }
+  ])
+
   return (
     <div className='user-bookings-page'>
-      <div className='card no-bookings'>
-        <p>Obs! Du har inte bokat ett pass! Besök sessionssidan och boka en plats idag!</p>
-      </div>
 
-      <div className="card booking">
-        <img className="image" src="/src/assets/images/girl-training.jpg" alt="Girl training."></img>
-        <div className="card-content-group">
-            <div className="title">
-              <h3>Morning Mobility</h3>
-            </div>
-            <div className='date-time'>
-              <p className='date'>02/10/2025</p>
-              <p className='time'>07:00 - 08:00</p>
-            </div>
+      <h1 className='bookings-title'>Dina bokade pass</h1>
+      {bookings.length === 0 ? (
+        <div className='card no-bookings'>
+          <p>Obs! Du har inte bokat ett pass! Besök sessionssidan och boka en plats idag!</p>
         </div>
-      </div>
+      )
+    : (
+        <div className='bookings-grid'>
+          {bookings.map(booking => {
+            return (
+              <div key={booking.id} className="card booking">
+                <img className="image" src="/src/assets/images/girl-training.jpg" alt="Girl training."></img>
+                <div className='booking-info'>
+                  <h3 className='booking-event-title'>{booking.title}</h3>
+                  <div className='date-time'>
+                    <p className='booking-date'>{booking.date}</p>
+                    <p className='booking-time'>{booking.time}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+          </div> 
+        )}
     </div>
   )
 }
